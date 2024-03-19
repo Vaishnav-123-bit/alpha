755a1e02c46801d3fc39df25ad2d356332b5a600
vaishnav mahdik <1032210456@tcetmumbai.in>
2024-03-19 23:41:25 +0530

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Post from './components/Post'
+import Header from './components/Header'
+import { Route ,Routes} from 'react-router'
+import Layout from './Layout'
+import Homepage from './pages/Homepage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import { UserContextProvider } from './UserContext'
+import CreatePost from './pages/CreatePost'
+import PostPage from './pages/PostPage'
+import EditPost from './pages/EditPost'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Homepage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/create' element={<CreatePost/>}/>
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+      </Route>
+    </Routes>
+
+    </UserContextProvider>
+    
+   
+
+      
+   
+  )
+}
+
+export default App
---
+++ client/src/UserContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react"
+
+export const Usercontext=createContext({})
+export function UserContextProvider({children}){
+    const [userInfo,setUserInfo]=useState({})
+    return(
+        <Usercontext.Provider value={{userInfo,setUserInfo}}>
+
+{children}
+        </Usercontext.Provider>
+            
+      
+    )
+}
